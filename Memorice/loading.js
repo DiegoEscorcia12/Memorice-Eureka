b6ec33d815c01074e4d248293558d390401532c6
DiegoEscorcia12 <diego52fee@gmail.com>
2024-09-09 10:47:35 -0500

    abrir

---
+++ Memorice/loading.js
@@ -0,0 +1,5 @@
+
+// Simula la carga y redirige después de 3 segundos (3000 ms)
+setTimeout(function() {
+    window.location.href = 'https://chatgpt.com/'; // Reemplaza con la URL del juego o sitio deseado
+}, 3000);
